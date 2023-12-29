b4df09f99d453f14ecddb015dd3f3a8222a3ac75
Clarsq13 <154024973+Clarsq13@users.noreply.github.com>
2023-12-28 22:03:16 -0300

    Add files via upload

---
+++ pokemon-model.js
@@ -0,0 +1,7 @@
+
+  let number = [1,2,3,4,5];
+  let name=["Bulbasaur","Blastoise","Charmander","Golbat","Nidoing"];
+  let types = ["fire","flying","plant","poison","water"];
+  let photo= ["https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/42.png","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/31.png"]
+  let abilities = [];
+console.log("#1" + "Bulbasaur"+"plant"+"poison"+"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png");
